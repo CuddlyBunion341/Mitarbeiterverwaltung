bbb3353ed373849823135bffaa5dd58801e2d807
CuddlyBunion341 <daniel@bengl.cz>
2022-06-16 15:23:10 +0200

    created Person model class

--- src/model/employees/Person.java
+++ src/model/employees/Person.java
@@ -1,4 +1,46 @@
 package model.employees;
 
 public class Person {
+    private String photoPath;
+    private String firstName;
+    private String lastName;
+    private Participation participation;
+
+    public Person(String fName, String lName, String photoPath) {
+        this.firstName = fName;
+        this.lastName = lName;
+        this.photoPath = photoPath;
+    }
+
+    public String getPhotoPath() {
+        return this.photoPath;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Participation getParticipation() {
+        return this.participation;
+    }
+
+    public void setParticipation(Participation participation) {
+        this.participation = participation;
+    }
 }
