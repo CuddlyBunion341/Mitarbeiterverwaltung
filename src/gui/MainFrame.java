1eadd8f333934611b9ebfe7e39612c92a962e42d
CuddlyBunion341 <daniel@bengl.cz>
2022-06-18 14:09:04 +0200

    alle tabbs bearbeitet

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -8,6 +8,9 @@ public class MainFrame extends JFrame {
     private JTabbedPane tabbedPanel;
     private UebersichtPanel uebersichtPanel;
     private ZuordnungPanel zuordnungPanel;
+    private PersonenPanel personenPanel;
+    private StammdatenPanel stammdatenPanel;
+    private LogBuchPanel logBuchPanel;
 
     public MainFrame() {
         setTitle("Hello World");
@@ -22,8 +25,14 @@ public class MainFrame extends JFrame {
         tabbedPanel = new JTabbedPane();
         uebersichtPanel = new UebersichtPanel();
         zuordnungPanel = new ZuordnungPanel();
+        personenPanel = new PersonenPanel();
+        stammdatenPanel = new StammdatenPanel();
+        logBuchPanel = new LogBuchPanel();
         tabbedPanel.addTab("Uebersicht", uebersichtPanel);
         tabbedPanel.addTab("Zuordnung", zuordnungPanel);
+        tabbedPanel.addTab("Personen", personenPanel);
+        tabbedPanel.addTab("Stammdaten", stammdatenPanel);
+        tabbedPanel.addTab("LogBuch", logBuchPanel);
         tabbedPanel.setForeground(Color.BLACK);
         add(tabbedPanel);
         getContentPane().add(tabbedPanel);
@@ -174,5 +183,164 @@ class ZuordnungPanel extends JPanel {
         abteilungFeld = new JTextField();
         funktionBox = new JComboBox<>(new String[]{"Controller","Betriebs-Sani"});
         teamBox = new JComboBox<>(new String[]{"More cash","New Customer","Idea 3000","Leitbild"});
+    }
+}
+
+class PersonenPanel extends JPanel {
+    private JList personenListe;
+    private JTextField nameFeld;
+    private JCheckBox hrCheckBox;
+    private JCheckBox adminCheckBox;
+    private JButton addBtn;
+    private JButton loeschBtn;
+    private JButton bearbeitBtn;
+
+    public PersonenPanel() {
+        super();
+        init();
+
+        setLayout(new BorderLayout());
+        JPanel mainPanel = Util.fieldset("Personen bearbeiten");
+        mainPanel.setLayout(new BorderLayout());
+        ListenBearbeitungPanel auswahlPanel = new ListenBearbeitungPanel(personenListe,"Übersicht");
+
+        JPanel detailPanel = Util.fieldset("Detail");
+        detailPanel.setLayout(new BoxLayout(detailPanel, BoxLayout.Y_AXIS));
+        JPanel detailOben = new JPanel(new GridLayout(1,2));
+        detailOben.add(new JLabel("Name: "));
+        detailOben.add(nameFeld);
+        detailPanel.add(detailOben);
+        detailPanel.add(hrCheckBox);
+        detailPanel.add(adminCheckBox);
+
+        mainPanel.add(auswahlPanel, BorderLayout.WEST);
+        mainPanel.add(detailPanel, BorderLayout.CENTER);
+        add(mainPanel, BorderLayout.CENTER);
+    }
+
+    private void init() {
+        personenListe = new JList<>(new String[]{"Person 1", "Person 2", "Person 3"});
+        nameFeld = new JTextField();
+        hrCheckBox = new JCheckBox("HR");
+        adminCheckBox = new JCheckBox("Admin");    
+        addBtn = new JButton("+");
+        loeschBtn = new JButton("x");
+        bearbeitBtn = new JButton("E");
+    }
+}
+
+class StammdatenPanel extends JPanel {
+    private JTextField firmaFeld;
+    private JList abteilungenListe;
+    private JList funktionenListe;
+    private JList teamsListe;
+    
+    private ListenBearbeitungPanel abteilungenPanel;
+    private ListenBearbeitungPanel funktionenPanel;
+    private ListenBearbeitungPanel teamsPanel;
+
+    public StammdatenPanel() {
+        super();
+        init();
+
+        setLayout(new GridLayout(4,2));
+        add(new JLabel("Firma: "));
+        add(firmaFeld);
+        add(new JLabel("Abteilungen: "));
+        add(abteilungenPanel);
+        add(new JLabel("Funktionen: "));
+        add(funktionenPanel);
+        add(new JLabel("Teams: "));
+        add(teamsPanel);
+    }
+
+    private void init() {
+        firmaFeld = new JTextField();
+        abteilungenListe = new JList<>(new String[]{"Logistik", "IT", "Marketing", "Produktion"});
+        funktionenListe = new JList<>(new String[]{"Controller", "Betriebs-Sani"});
+        teamsListe = new JList<>(new String[]{"More cash", "New Customer", "Idea 3000", "Leitbild"});
+
+        abteilungenPanel = new ListenBearbeitungPanel(abteilungenListe);
+        funktionenPanel = new ListenBearbeitungPanel(funktionenListe);
+        teamsPanel = new ListenBearbeitungPanel(teamsListe);
+    }
+
+}
+
+class LogBuchPanel extends JPanel {
+    private JTextArea logBuch;
+
+    public LogBuchPanel() {
+        super();
+        setLayout(new BorderLayout());
+        logBuch = new JTextArea();
+        for (int i = 0; i < 50; i++) {
+            logBuch.append("[2022-05-07 11:32:16.113] Logbuch Eintrag Test " + i + "\n");
+        }
+        logBuch.setEditable(false);
+        logBuch.setFont(new Font("Arial", Font.PLAIN, 16));
+        JScrollPane scrollPane = new JScrollPane(logBuch);
+        add(scrollPane, BorderLayout.CENTER);
+    }
+}
+
+class ListenBearbeitungPanel extends JPanel {
+    private JList liste;
+    private JButton addBtn;
+    private JButton bearbeitBtn;
+    private JButton loeschBtn;
+
+    public ListenBearbeitungPanel(JList liste) {
+        super();
+        this.liste = liste;
+        init();
+        setLayout(new BorderLayout());
+        JScrollPane scrollPane = new JScrollPane(liste);
+        add(scrollPane, BorderLayout.CENTER);
+        addBtns();
+    }
+
+    private void addBtns() {
+        JPanel btnJPanel = new JPanel(new GridLayout(1,3));
+        btnJPanel.add(addBtn);
+        btnJPanel.add(bearbeitBtn);
+        btnJPanel.add(loeschBtn);
+        add(btnJPanel, BorderLayout.PAGE_END);
+    }
+
+    public ListenBearbeitungPanel(JList liste, String titel) {
+        super();
+        this.liste = liste;
+        init();
+        setLayout(new BorderLayout());
+        JScrollPane scrollPane = Util.scrollPane(titel, liste);
+        add(scrollPane, BorderLayout.CENTER);
+        addBtns();
+    }
+
+    private void init() {
+        addBtn = new JButton("+");
+        loeschBtn = new JButton("x");
+        bearbeitBtn = new JButton("b");
+    }
+
+    public int getSelectedIndex() {
+        return liste.getSelectedIndex();
+    }
+
+    public Object getSelectedValue() {
+        return liste.getSelectedValue();
+    }
+
+    public JButton getAddBtn() {
+        return addBtn;
+    }
+
+    public JButton getBearbeitBtn() {
+        return bearbeitBtn;
+    }
+
+    public JButton getLoeschBtn() {
+        return loeschBtn;
     }
 }
