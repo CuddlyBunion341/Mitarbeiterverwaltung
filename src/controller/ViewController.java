758bfa5cade9eb06adc55a8da7ba350c4f4b5e06
Francesco Feroldi <feroldif@bzz.ch>
2022-06-20 21:19:09 +0200

    login GUI

--- src/controller/ViewController.java
+++ src/controller/ViewController.java
@@ -8,12 +8,37 @@ import view.LoggedIn;
 import view.LoggedOut;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ViewController {
+public class ViewController extends JFrame implements ActionListener {
+    Login login = new Login();
+
     public void start(){
-        //ViewController controller = new ViewController();
-        //controller.start();
+        login.getLoginButton().addActionListener(this);
+        login.getResetButton().addActionListener(this);
+        login.getShowPassword().addActionListener(this);
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == login.getLoginButton()) {
+
+        }
+        if (e.getSource() == login.getResetButton()) {
+            login.getUserField().setText("");
+            login.getPasswordField().setText("");
+        }
+        if (e.getSource() == login.getShowPassword()) {
+            if (login.getShowPassword().isSelected()) {
+                login.getPasswordField().setEchoChar((char) 0);
+            } else {
+                login.getPasswordField().setEchoChar('*');
+            }
+        }
+    }
+
+    public void initializeMainFrame(){
         Company model = new Company("Mitarbeiterverwaltung v2.31");
         MainFrame frame = new MainFrame();
         frame.setVisible(true);
