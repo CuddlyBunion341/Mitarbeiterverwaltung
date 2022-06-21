cbb2bd9af63fc20640d14aa1d8018a2bbc71f303
Francesco Feroldi <feroldif@bzz.ch>
2022-06-21 09:56:53 +0200

    worked on view controller

--- src/controller/ViewController.java
+++ src/controller/ViewController.java
@@ -14,6 +14,7 @@ import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.RectangularShape;
 import java.util.Locale;
 import java.util.Vector;
 
@@ -38,7 +39,11 @@ public class ViewController extends JFrame implements ActionListener {
         frame.getOverviewTab().getEmployeeList().setListData(company.getEmployees());
         frame.getOverviewTab().getFunctionList().setListData(new Vector());
         frame.getOverviewTab().getTeamList().setListData(new Vector());
+        frame.getEmployeeTab().getDelBtn().addActionListener(this);
+        frame.getDataTab().getDepartmentPanel().getAddBtn().addActionListener(this);
         frame.getOverviewTab().getEmployeeList().addListSelectionListener(new listSelection());
+        frame.getOverviewTab().getDepartmentFilter().addActionListener(this);
+        frame.getEmployeeTab().getAddBtn().addActionListener(this);
         frame.getOverviewTab().getNameFilter().getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
@@ -59,7 +64,7 @@ public class ViewController extends JFrame implements ActionListener {
                 else{
                     Vector<Person> result = new Vector<Person>();
                     for (int i = 0; i < company.getEmployees().size(); i++){
-                        if (company.getEmployees().get(i).getName().toLowerCase(Locale.ROOT).contains(frame.getOverviewTab().getNameFilter().getText())){
+                        if (company.getEmployees().get(i).getName().toLowerCase(Locale.ROOT).contains(frame.getOverviewTab().getNameFilter().getText().toLowerCase(Locale.ROOT))){
                             result.add(company.getEmployees().get(i));
                         }
                     }
@@ -71,6 +76,8 @@ public class ViewController extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        Vector<Person> result = company.getEmployees();
+        Vector<Person> result2;
         if (e.getSource() == login.getLoginButton()) {
             for (int i = 0; i < company.getEmployees().size(); i++){
                 if (login.getUserField().getText().equals(company.getEmployees().get(i).getName())){
@@ -94,6 +101,68 @@ public class ViewController extends JFrame implements ActionListener {
                 login.getPasswordField().setEchoChar('*');
             }
         }
+
+        //Filter nach Department
+        if (e.getSource() == frame.getOverviewTab().getDepartmentFilter()){
+            if (frame.getOverviewTab().getDepartmentFilter().getSelectedItem().toString().equals("Alle")){
+                result = company.getEmployees();
+            }
+            else{
+                result = null;
+                result = new Vector<Person>();
+                for (int i = 0; i < company.getEmployees().size(); i++){
+                    if (company.getEmployees().get(i).getDepartment().equals(frame.getOverviewTab().getDepartmentFilter().getSelectedItem().toString())){
+                        result.add(company.getEmployees().get(i));
+                    }
+                }
+            }
+            frame.getOverviewTab().getEmployeeList().setListData(result);
+        }
+
+        //Filter nach Team
+        if (e.getSource() == frame.getOverviewTab().getTeamFilter()){
+            result2 = new Vector<Person>();
+            if (frame.getOverviewTab().getTeamFilter().getSelectedItem().toString().equals("Alle")){
+                result2 = result;
+            }
+            else{
+                for (int i = 0; i < result.size(); i++){
+                    //if (result.get(i).get)
+                }
+            }
+        }
+
+        //Andere Filter
+        if (e.getSource() == frame.getEmployeeTab().getAddBtn()){
+            String[] splited = frame.getEmployeeTab().getNameField().getText().split(" ");
+            Person person = new Person(splited[0], splited[1], "");
+            if (frame.getEmployeeTab().getAdminCheckBox().isSelected()){
+                person.setAdmin(true);
+            }
+            if (frame.getEmployeeTab().getHrCheckBox().isSelected()){
+                person.setHr(true);
+            }
+            company.getEmployees().add(person);
+            frame.getEmployeeTab().getEmployeeList().setListData(company.getEmployees());
+        }
+        if (e.getSource() == frame.getEmployeeTab().getDelBtn()){
+            Person person = company.getEmployees().get(frame.getEmployeeTab().getEmployeeList().getSelectedIndex());
+            company.getEmployees().remove(person);
+            frame.getEmployeeTab().getEmployeeList().setListData(company.getEmployees());
+        }
+        if (e.getSource() == frame.getDataTab().getDepartmentPanel().getAddBtn()){
+            //company.getDepartments().add(frame.getDataTab().getDepartmentPanel().get);
+        }
+        /*
+        if (e.getSource() == frame.getDataTab().g){
+            //company.getDepartments().add(frame.getDataTab().getDepartmentPanel().getName());
+        }
+        if (e.getSource() == frame.getDataTab().getAddBtn){
+            //company.getDepartments().add(frame.getDataTab().getDepartmentPanel().getName());
+        }
+
+         */
+
     }
 
     class listSelection implements ListSelectionListener{
@@ -102,9 +171,17 @@ public class ViewController extends JFrame implements ActionListener {
             try{
                 Person person = company.getEmployees().get(frame.getOverviewTab().getEmployeeList().getSelectedIndex());
                 frame.getOverviewTab().getNameField().setText(person.getName());
-                //frame.getOverviewTab().getDepartmentField().setText(person.getDepartment);
+                frame.getOverviewTab().getNameField().updateUI();
+
+               /*
+                frame.getOverviewTab().getDepartmentField().setText(person.getDepartment());
+                frame.getOverviewTab().getDepartmentField().updateUI();
                 frame.getOverviewTab().getFunctionList().setListData(person.getFunctions());
+                frame.getOverviewTab().getFunctionList().updateUI();
                 frame.getOverviewTab().getTeamList().setListData(person.getTeams());
+                frame.getOverviewTab().getTeamList().updateUI();
+                 */
+
             }
             catch (ArrayIndexOutOfBoundsException ignored){}
         }
